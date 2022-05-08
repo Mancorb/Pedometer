c3819ae109e31d300237f6a69b73d159a014c77f
Mancorb <andres.m.bosques@gmail.com>
2022-05-08 15:19:00 -0500

    Clean up and 24hr timer experiment
    Added:
    24hr clock with no restart to test if the sensor will keep running.
    
    Changes:
    changes the location of methods and made the code a bit more cleaner.
    
    If this experiment fails do the following:
    
    1.- when the app stops:
    
    Save total ammount of steps in shared preferences
    
    Save in shard preferences the current time
    
    2.- When the app resumes or is created again
    
    Check current time and calculate ammount of hours in between  (in the length of 10min spaces of time)
    
    Add into the database the missing registries with 0 steps as default value
    
    Continue normal operations

--- PedometerAtempt1/app/src/main/java/com/example/pedometeratempt1/MainActivity.java
+++ PedometerAtempt1/app/src/main/java/com/example/pedometeratempt1/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.pedometeratempt1;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
@@ -18,6 +21,7 @@ import android.hardware.SensorManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
@@ -30,33 +34,23 @@ import java.util.Calendar;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
-    //Ammount of time for timmer 600000 = 10 min
-    private static final long START_TIME_IN_MILLIS = 600000;
-    //Set the stablished ammount of time to the counter variable
-    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
-    //Count down obj
-    private CountDownTimer mCountdowntimer;
-    //String to store current date
-    private String CurDate;
-    //String to store current hour
-    private String CurHour;
+    private TextView  StepDetectVw, CountDownTV, TotalStepVw;//UI conections
+    private String CurDate,CurHour;//Store date
+    int stepsDtected = 0, stepCounter =0;//Counters for UI
+    private SensorManager sensorManager;//Sensor obj
+    private Sensor  nStepDetector;//Step detector obj
+    private boolean  stepDetectorAvailability;//sensor bool
+    private static final long START_TIME_IN_MILLIS = 600000;//Amount of time for timer 600000 = 10 min
+    private long timeleft=86400000;
+    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;//Store current time left
+    private CountDownTimer mCountdowntimer;//CountDown obj
 
-    private TextView  StepDetectVw, CountDownTV, TotalStepVw;
-    private SensorManager sensorManager;
-    private Sensor  nStepDetector;
-    private boolean  stepDetectorAvailability;
-    int stepsDtected = 0, stepCounter =0;
-
+    @SuppressLint({"SourceLockedOrientationActivity", "InlinedApi"})
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //----------------------------------------------------------------
-        //Avoid it from rotating since it restarts the countdown
-        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        //----------------------------------------------------------------
-
 
         //Conect with UI
         CountDownTV=(TextView)findViewById(R.id.CountDowntTxt);
@@ -64,46 +58,24 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         TotalStepVw=(TextView)findViewById(R.id.TotalStepsTV);
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
 
-        //Ask for permission to access pedometer sensor
-        if(ContextCompat.checkSelfPermission(this,
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);//maintain portrait view
+
+        if(ContextCompat.checkSelfPermission(this, //Obtain sensor aproval
                 Manifest.permission.ACTIVITY_RECOGNITION) == PackageManager.PERMISSION_DENIED){
             requestPermissions(new String[]{Manifest.permission.ACTIVITY_RECOGNITION}, 0);
         }
-        //Stop phone from sleeping
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-
-        //Change bool (true = pedometer available)
-        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR)!=null){
-            nStepDetector = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
-            stepDetectorAvailability = true;
-        }
-        else{
-            StepDetectVw.setText("No se puede acceder al sensor");
-            stepDetectorAvailability = false;
-        }
-        //----------------------------------------------------------------
+        //run pedometer bool
+        pedometerAvailability();
+        //Obtain latest info from phone
         updateInfo();
         //Run the countdown
+        stayAliveCountDown();
         countDown();
 
     }
-    //---------------------------------------
-    //Obtain cumulative ammount of steps
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-        if (sensorEvent.sensor == nStepDetector){
-            stepCounter++;
-            stepsDtected = (int) (stepsDtected+sensorEvent.values[0]);
-            TotalStepVw.setText(String.valueOf(stepsDtected));
-            StepDetectVw.setText(String.valueOf(stepCounter));
 
-        }
-    }
-    //what to do when the accuracy changes (Obligatory)
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
 
-    }
+
 
     //What to do when the app resumes
     @Override
@@ -114,21 +86,32 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
     }
 
-    //--------------------------------------
-    //Restart the count down with original ammount of miliseconds as well as call again the timmer method
-    public void timeFinnished (){
-        //Toast.makeText(this, "Time is up", Toast.LENGTH_SHORT).show();
-        mTimeLeftInMillis = START_TIME_IN_MILLIS;
-        registrarDatos();
-        countDown();
+
+    public void stayAliveCountDown (){
+        //countdown 86,400,000
+
+        mCountdowntimer = new CountDownTimer(timeleft, 1000) {
+
+            @Override
+            public void onTick(long millsUntilFinished) {
+                timeleft = millsUntilFinished;
+            }
+
+            @Override
+            public void onFinish() {
+                Toast.makeText(MainActivity.this, "sucess", Toast.LENGTH_SHORT).show();
+            }
+        }.start();
     }
+
     // Uses certain ammount of miliseconds and counts down (it reflects the equivalent time in screen as minutes and seconds)
     public void countDown (){
         //countdown
         mCountdowntimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+
             @Override
-            public void onTick(long millsUntilFinnished) {
-                mTimeLeftInMillis = millsUntilFinnished;
+            public void onTick(long millsUntilFinished) {
+                mTimeLeftInMillis = millsUntilFinished;
                 int minutes = (int) (mTimeLeftInMillis/1000) /60;
                 int seconds = (int) (mTimeLeftInMillis/1000) %60;
                 String timeLeftFormated = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
@@ -141,7 +124,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             }
         }.start();
     }
-    //------------------------------------
+
+    //Restart the count down with original ammount of miliseconds as well as call again the timmer method
+    public void timeFinnished (){
+        //Toast.makeText(this, "Time is up", Toast.LENGTH_SHORT).show();
+        mTimeLeftInMillis = START_TIME_IN_MILLIS;
+        registrarDatos();
+        countDown();
+    }
+
     public void registrarDatos (){
         DataModel dataModel;
         try{
@@ -169,4 +160,38 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         SimpleDateFormat format  = new SimpleDateFormat("HH:mm");
         this.CurHour = format.format(calendar.getTime());
     }
+
+    //Obtain cumulative ammount of steps
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor == nStepDetector){
+            stepCounter++;
+            stepsDtected = (int) (stepsDtected+sensorEvent.values[0]);
+            TotalStepVw.setText(String.valueOf(stepsDtected));
+            StepDetectVw.setText(String.valueOf(stepCounter));
+
+        }
+    }
+
+
+    //---------------------------------------
+
+    public void pedometerAvailability(){
+        //Change bool (true = pedometer available)
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR)!=null){
+            nStepDetector = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+            stepDetectorAvailability = true;
+        }
+        else{
+            StepDetectVw.setText("No se puede acceder al sensor");
+            stepDetectorAvailability = false;
+        }
+    }
+    //what to do when the accuracy changes (Obligatory)
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
 }
